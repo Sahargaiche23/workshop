d20d597b860fed3c532cd025b17bcce1b07ac74a
Sahargaiche23 <sahargaiche6@gmail.com>
2024-09-30 15:49:32 +0100

    workshop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListCategoriesComponent } from './components/list-categories/list-categories.component';
+import { FormsModule } from '@angular/forms';
+import { AppHighlightDirective } from './app-highlight.directive';
+import { SearchPipe } from './search.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListCategoriesComponent,
+    AppHighlightDirective,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
